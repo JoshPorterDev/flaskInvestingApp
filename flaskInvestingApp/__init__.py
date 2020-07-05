22763470aabfb7177f21cd774fe512b0ab8fab9b
JoshPorterDev <porterjosh417@gmail.com>
2020-07-04 23:55:42 -0500

    project settings and config

--- flaskInvestingApp/__init__.py
+++ flaskInvestingApp/__init__.py
@@ -1,15 +1,23 @@
 """Initialize Flask App."""
+import os
 from flask import Flask
+from flaskInvestingApp.config import Config
+from dotenv import load_dotenv
 # from flask_sqlalchemy import SQLAlchemy
 
 # Globally accessible libraries
 # db = SQLAlchemy()
 
 
-def create_app():
+def create_app(config=Config):
     """Create Flask app."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # bring in .env file
+    APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
+    dotenv_path = os.path.join(APP_ROOT, '.env')
+    load_dotenv(dotenv_path)
 
     # Initialize plugins
     # db.init_app(app)
@@ -20,5 +28,7 @@ def create_app():
 
         # Register blueprints
         app.register_blueprint(home.home_bp)
+        print(app.secret_key)
+        print(app.debug)
 
         return app
